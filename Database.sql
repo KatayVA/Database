6ff1a60f7f0376276a193e0b30c9e929d2350e0c
Katay <karolina3009@yandex.ru>
2025-04-09 00:42:50 +0500

    change Database

--- Database.sql
+++ Database.sql
@@ -1,10 +1,10 @@
-create table IF not exists Genres (
+create table if not exists Genres (
 id SERIAL primary key,
 genre_name VARCHAR(80) not NULL
 );
 
 create table if not exists Perfomers (
-id SERIAL primary key,
+id SERIAL primary key references Genres(id),
 performer_name VARCHAR(80) not null
 );
 
@@ -17,25 +17,26 @@ constraint pk primary key
 create table if not exists Albums (
 id SERIAL primary key,
 album_name VARCHAR(80) not null,
-release_year date
+release_year date check(release_year>=1950) not null
 );
 
 create table if not exists PerformerAlbum (
 id SERIAL primary key,
 album_id INTEGER not null references Albums(id),
 perfomers_id INTEGER not null references Perfomers(id)
+constraint pk primary key
 );
 
 create table if not exists Tracks (
 id INTEGER primary key references Albums(id),
 track_name VARCHAR(80) not null,
-duration_track INTEGER not null time(second<=300)  
+duration_second_track INTEGER check(duration_second_track<=1200) not null    
 );
 
 create table if not exists Collections (
 id SERIAL primary key,
 collection_name VARCHAR(80) not null,
-release_year_collection date
+release_year_collection date check(release_year_collection>=1950) not null
 );
 
 create table if not exists CollectionTrack (
